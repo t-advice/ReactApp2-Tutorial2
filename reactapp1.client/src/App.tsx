d7c75814daca849c330d0af168272d0d4a9d1075
Tashwill Advice <tashwilladvice02@gmail.com>
2026-01-28 14:31:21 +0200

    butoon added with funcionality

--- reactapp1.client/src/App.tsx
+++ reactapp1.client/src/App.tsx
@@ -5,12 +5,20 @@ function App() {
     const jobTitle = "Junior Software Developer"; // Example variable
     const currentDate = new Date().toLocaleDateString();  // Get current date
 
+    function handleLogin() {
 
+        alert("Login clicked! Connecting to server");
+    }
     // the view ( UI )
     return (
         <div className="app-container">
             <h1>Login  as  {jobTitle}!</h1>
             <p>Today is: {currentDate}</p>
+
+            <button className="login-btn" onClick={handleLogin}>
+                Log In
+            </button>
+
         </div>
     );
 }
