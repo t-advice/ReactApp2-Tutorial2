1f7fac83ee20b3c524c81f1515ab515ec04db66d
Tashwill Advice <tashwilladvice02@gmail.com>
2026-02-02 15:26:27 +0200

    created the ToDo Class with cde below  public int Id { get; set; } public string? Title { get; set; } public bool IsCompleted { get; set; }

---
+++ ReactApp1.Server/Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿namespace ReactApp1.Server.Models
+{
+    public class TodoItem
+    {
+        // The Primary Key(ID)
+        public int Id { get; set; }
+
+        // The actual task text ( lets say "Buy groceries" )
+        public string? Title { get; set; }
+
+        // Is it done yet?
+        public bool IsCompleted { get; set; }
+
+    }
+}
