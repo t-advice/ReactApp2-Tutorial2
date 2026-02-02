610ed15436d89ede407ad5f73539e9d7d641e454
Tashwill Advice <tashwilladvice02@gmail.com>
2026-02-02 15:32:51 +0200

    // Inherit from DbContext to create a database context for EF Core created a Data folder and a class named DbContext to create a table called " TodoItem"

---
+++ ReactApp1.Server/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReactApp1.Server.Models;
+
+
+namespace ReactApp1.Server.Data
+{
+    // Inherit from DbContext to create a database context for EF Core
+    public class AppDbContext : DbContext
+    {
+        // This DbSet represents the collection of TodoItem entities in the database
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        {
+
+        }
+
+        // This line tells EF Core: "I want a table in the DB called 'TodoItems'"
+        public DbSet<TodoItem> TodoItems { get; set; } 
+        
+    }
+}
